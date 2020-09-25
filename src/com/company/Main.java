69b11e484d0fbb3e3692dae8f30a8d081e3e78cc
lh <lh@163.com>
2020-09-25 11:47:53 +0800

    提交项目到本地库

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    /**
+     * 1.好的就是这么回事
+     * 2.这是什么事情啊？
+     * 这么点事情就处理不好啦？
+     *
+     */
+    public static void main(String[] args) {
+	  //你好
+    }
+}
